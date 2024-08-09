2055486eed162c1aa8aa6058754bbea137782e63
DevDvt <devangthakar02@gmail.com>
2024-08-09 12:28:21 +0530

    first commit

---
+++ src/test/java/swjmeaven/first.java
@@ -0,0 +1,13 @@
+package swjmeaven;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class first {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/");
+    }
+}
